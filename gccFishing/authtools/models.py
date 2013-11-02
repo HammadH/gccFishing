6f9481bb48292485ae7515115eaa0c052ea5a437
Hammad <hammadsyed9@gmail.com>
2013-11-02 04:20:32 +0300

    made is_active true and removed image, country and city from requirements in authtools models

--- gccFishing/authtools/models.py
+++ gccFishing/authtools/models.py
@@ -87,7 +87,7 @@ class AbstractEmailUser(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(_('staff status'), default=False,
         help_text=_('Designates whether the user can log into this admin '
                     'site.'))
-    is_active = models.BooleanField(_('active'), default=False,
+    is_active = models.BooleanField(_('active'), default=True,
         help_text=_('Designates whether this user should be treated as '
                     'active.  Unselect this instead of deleting accounts.'))
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
@@ -163,7 +163,7 @@ class AbstractNamedUser(AbstractEmailUser):
 
     
 
-    REQUIRED_FIELDS = ['name', 'image', 'country', 'city']
+    REQUIRED_FIELDS = ['name',]
 
     class Meta:
         abstract = True
