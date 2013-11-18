db42d96195703bfc3c17022837cfd145e8f7fca8
Hammad <hammadsyed9@gmail.com>
2013-11-18 21:24:33 +0300

    cleaing citywall

--- gccFishing/gccFishing/settings.py
+++ gccFishing/gccFishing/settings.py
@@ -17,10 +17,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'new_db',                      # Or path to database file if using sqlite3.
+        'NAME': 'gccfishingdb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'hammad',
-        'PASSWORD': '1234',
+        'PASSWORD': 'quakeroats9',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  
         'OPTIONS': {'autocommit': True,},                    # Set to empty string for default.
