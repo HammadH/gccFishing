d5349dcb94a0347fa8c6c8b3b753029f94f22157
Hammad <hammadsyed9@gmail.com>
2013-12-15 21:18:36 +0300

    wsgi_prod

--- gccFishing/gccFishing/wsgi_prod.py
+++ gccFishing/gccFishing/wsgi_prod.py
@@ -1,4 +1,21 @@
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gccfishing.settings_prod")
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import sys
+
+sys.path.insert(0, '/home/hammad/webapps/gccfishing/gccFishing/gccFishing')
+
+from gccFishing import settings_prod
+
+import django.core.management
+django.core.management.setup_environ(settings_prod)
+utility = django.core.management.ManagementUtility()
+command = utility.fetch_command('runserver')
+
+command.validate()
+
+import django.conf
+import django.utils
+
+django.utils.translation.activate(django.conf.settings.LANGUAGE_CODE)
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
