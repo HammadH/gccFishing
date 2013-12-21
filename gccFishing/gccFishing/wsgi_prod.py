6a88a6998e7eb63951cd380ccf74bda8131419a5
Hammad <hammadsyed9@gmail.com>
2013-12-21 20:11:20 +0300

    changed wsgi_prod.py

--- gccFishing/gccFishing/wsgi_prod.py
+++ gccFishing/gccFishing/wsgi_prod.py
@@ -1,21 +1,32 @@
-import sys
+"""
+WSGI config for gccFishing project.
 
-sys.path.insert(0, '/home/hammad/webapps/gccfishing/gccFishing/gccFishing')
+This module contains the WSGI application used by Django's development server
+and any production WSGI deployments. It should expose a module-level variable
+named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
+this application via the ``WSGI_APPLICATION`` setting.
 
-from gccFishing import settings_prod
+Usually you will have the standard Django WSGI application here, but it also
+might make sense to replace the whole Django WSGI application with a custom one
+that later delegates to the Django one. For example, you could introduce WSGI
+middleware here, or combine a Django application with an application of another
+framework.
 
-import django.core.management
-django.core.management.setup_environ(settings_prod)
-utility = django.core.management.ManagementUtility()
-command = utility.fetch_command('runserver')
+"""
+import os
 
-command.validate()
+# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
+# if running multiple sites in the same mod_wsgi process. To fix this, use
+# mod_wsgi daemon mode with each site in its own daemon process, or use
+# os.environ["DJANGO_SETTINGS_MODULE"] = "gccFishing.settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gccFishing.settings_prod")
 
-import django.conf
-import django.utils
+# This application object is used by any WSGI server configured to use this
+# file. This includes Django's development server, if the WSGI_APPLICATION
+# setting points here.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
 
-django.utils.translation.activate(django.conf.settings.LANGUAGE_CODE)
-
-import django.core.handlers.wsgi
-
-application = django.core.handlers.wsgi.WSGIHandler()
+# Apply WSGI middleware here.
+# from helloworld.wsgi import HelloWorldApplication
+# application = HelloWorldApplication(application)
